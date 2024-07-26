c8ae70701925e1644041ec3b64ce72a495b8aa3c
Jarret Cannon <jarret@Jarrets-Mac-mini-2.local>
2024-07-25 21:55:59 -0700

    changed heroku config

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("zealthyDatabase", "root", "MartinD-15", {
-  host: "localhost",
-  dialect: "mysql",
+const databaseUrl = process.env.JAWSDB_URL || "mysql://root:MartinD-15@localhost:3306/zealthyDatabase";
+
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'mysql',
+  logging: false, 
 });
 
 async function initializeDatabase() {
@@ -13,6 +15,7 @@ async function initializeDatabase() {
     console.error("Unable to connect to the database:", err);
   }
 }
+
 initializeDatabase();
 
 export default sequelize;
