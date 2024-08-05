2de5bf7b13f3c530bbbbe80cc3ee1b2b9eced7e1
Jarret Cannon <jarretcannon@gmail.com>
2024-08-04 22:46:48 -0700

    .env file added

--- api/src/models/index.ts
+++ api/src/models/index.ts
@@ -1,8 +1,13 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("zealthyDatabase", "root", "MartinD-15", {
-  host: "localhost",
-  dialect: "mysql",
+const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 async function initializeDatabase() {
@@ -13,6 +18,7 @@ async function initializeDatabase() {
     console.error("Unable to connect to the database:", err);
   }
 }
+
 initializeDatabase();
 
 export default sequelize;
